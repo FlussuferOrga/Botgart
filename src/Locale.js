cf137148dfb316ee9d54f98436e99b65942d17c8
Daniel O'Grady <danielogrady@gmx.de>
2018-11-14 16:46:13 +0100

    Add several locale strings

--- src/Locale.js
+++ src/Locale.js
@@ -1,8 +1,10 @@
 var DE = {
 	"NO_DEL_PERM": "Ich kann leider keine Nachrichten löschen. Bitte entferne aus Sicherheitsgründen deine letzte Nachricht manuell.",
-	"KEY_INVALID_FORMAT": "Bitte gib mir einen gültigen API-key. Diesen erhältst du auf https://account.arena.net/.",
+	"KEY_INVALID_FORMAT": "Bitte gib mir einen gültigen API-Key. Diesen erhältst du auf https://account.arena.net/.",
 	"KEY_ACCEPTED": "Herzlich willkommen, Flussufler!",
-	"KEY_DECLINED": "Der von dir angegebene Account ist derzeit nicht auf Flussufer angesiedelt."
+	"KEY_DECLINED": "Der von dir angegebene Account ist derzeit nicht auf Flussufer angesiedelt.",
+    "CHECKING_KEY": "Prüfe API-Key...",
+    "REG_ROLE_NOT_FOUND": "Die eingestellte Rolle existriert nicht auf dem Server. Bitte informiere einen Administrator."
 }
 
 exports.get = function(key) {
