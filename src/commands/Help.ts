99d3c6637d4189a801b914a2eb00aae3213999f3
Tobias Günther <Xyaren@users.noreply.github.com>
2021-02-12 15:33:49 +0100

    make Help a bit more readable

--- src/commands/Help.ts
+++ src/commands/Help.ts
@@ -1,24 +1,24 @@
 import * as discord from "discord.js";
-import { BotgartCommand } from "../BotgartCommand";
+import {BotgartCommand} from "../BotgartCommand";
 
 /**
-Testcases:
-- run in channel -> bot posts a list of commands
-- run in DM -> bot posts a list of commands
-- run per cron -> bot posts a list of commands
-- run with very long help text (change desc for some commands in Locale) > 2000 chars -> response comes in parts
-- run with one very long help text (change desc for one command in Locale) > 2000 chars -> that command is omitted
-*/
+ Testcases:
+ - run in channel -> bot posts a list of commands
+ - run in DM -> bot posts a list of commands
+ - run per cron -> bot posts a list of commands
+ - run with very long help text (change desc for some commands in Locale) > 2000 chars -> response comes in parts
+ - run with one very long help text (change desc for one command in Locale) > 2000 chars -> that command is omitted
+ */
 export class Help extends BotgartCommand {
     constructor() {
         super("help", {
-            aliases: ["help", "commands", "hilfe"],
-        }, 
-        {
-            availableAsDM: true,
-            cronable: true,
-            everyonePermission: 1
-        }
+                aliases: ["help", "commands", "hilfe"],
+            },
+            {
+                availableAsDM: true,
+                cronable: true,
+                everyonePermission: 1
+            }
         );
     }
 
@@ -32,16 +32,16 @@ export class Help extends BotgartCommand {
         const user: discord.GuildMember | discord.User = guild ? await guild.members.fetch(responsible.id) : responsible; //cache.find(m => m.id == responsible.id) : responsible;
         //let checkPermissions = member ? member.permissions.has.bind(member.permissions) : () => true;
         const descs = "**COMMANDS:**\n\n"
-                    .concat(Array.from(this.getBotgartClient().commandHandler.modules.values())
-                        .map(m => <BotgartCommand>m)
-                        .filter(m => m.isAllowed(user))
-                        .sort((m1,m2) => m1.id < m2.id ? -1 : 1)
-                        .map(m => m.desc 
-                        ? `**${m.id}**\n(${m.aliases.map(a => "`{0}`".formatUnicorn(a)).join(", ")}): ${m.desc()}\n-------`
-                        : m.id
-                    ).join(separator));
+            .concat(Array.from(this.getBotgartClient().commandHandler.modules.values())
+                .map(m => <BotgartCommand>m)
+                .filter(m => m.isAllowed(user))
+                .sort((m1, m2) => m1.id < m2.id ? -1 : 1)
+                .map(m => m.desc
+                    ? `**${m.id}**\n(${m.aliases.map(a => "`{0}`".formatUnicorn(a)).join(", ")}): ${m.desc()}\n_ _\n`
+                    : m.id
+                ).join(separator));
 
-        message.reply(descs, {split: true});
+        message.reply(descs, {split: {prepend: "_ _\n"},});
     }
 }
 
