92c237772876b66879fc4b310fe4efc59f7010ef
Tobias Günther <Xyaren@users.noreply.github.com>
2021-02-21 15:08:52 +0100

    change synced time format

--- src/commands/resetlead/RosterService.ts
+++ src/commands/resetlead/RosterService.ts
@@ -192,7 +192,7 @@ export class RosterService {
         }
         const resetDateTime = roster.getResetMoment().tz(getConfig().get().timeZone)
         await this.client.getTS3Connection().post("resetroster", {
-            "date": resetDateTime.format("dd.mm.yy"), //TODO: remove
+            "date": resetDateTime.format("DD.MM.YYYY HH:mm z"), //TODO: remove
             "datetime": resetDateTime.format(),
             "rbl": await Promise.all(Array.from(roster.getMapLeaders(WvwMap.RedBorderlands)).map(l => resolveUser(l.name))),
             "gbl": await Promise.all(Array.from(roster.getMapLeaders(WvwMap.GreenBorderlands)).map(l => resolveUser(l.name))),
