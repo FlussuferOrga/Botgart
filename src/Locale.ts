4bf87780c78ca5e743d92446aa454fcc93f59de0
Tobias Günther <Xyaren@users.noreply.github.com>
2021-03-31 23:36:47 +0200

    add swabian icon

--- src/Locale.ts
+++ src/Locale.ts
@@ -1,3 +1,4 @@
+import { GuildEmoji } from "discord.js";
 import * as U from "./Util";
 
 let EN : {[key:string]: string} = {
@@ -916,7 +917,7 @@ class Language {
 
 export const german = new Language("DE", ":flag_de:", DE);
 export const english = new Language("EN", ":flag_gb:", EN);
-export const swabian = new Language("DE_SWG", ":baw:", DE_SWG); // ":black_large_square::yellow_square:"
+export const swabian = new Language("DE_SWG", "<:flag_de_swg:826914314866262036>", DE_SWG); // ":black_large_square::yellow_square:"
 
 export const availableLanguages = [german, english, swabian];
 const currentLanguages: Language[] = [];
