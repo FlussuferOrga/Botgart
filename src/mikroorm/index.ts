11c2059ea6ad2fbd60b1f08196ccaa41b8ec8b89
Tobias Günther <Xyaren@users.noreply.github.com>
2023-07-13 14:34:35 +0200

    Disable db optimization for now

--- src/mikroorm/index.ts
+++ src/mikroorm/index.ts
@@ -9,6 +9,19 @@ export async function setupPersistence() {
     LOG.info("Connecting to DB");
     await orm.connect();
 
+    async function optimize() {
+        const knex = orm.em.getKnex();
+        // await knex.raw("PRAGMA analysis_limit=400");
+        // await knex.raw("PRAGMA optimize;");
+        await knex.raw("drop table if exists sqlite_stat1;");
+        await knex.raw("drop table if exists sqlite_stat2;");
+        await knex.raw("drop table if exists sqlite_stat3;");
+        await knex.raw("drop table if exists sqlite_stat4;");
+        LOG.debug("Done optimizing database");
+    }
+
+    await optimize();
+
     LOG.debug("Create Schema:\n\n " + (await orm.getSchemaGenerator().getCreateSchemaSQL()) + "\n\n");
 
     const migrator = orm.getMigrator();
@@ -21,14 +34,9 @@ export async function setupPersistence() {
         throw Error("Database Migration is required");
     }
 
-    async function optimize() {
-        const knex = orm.em.getKnex();
-        await knex.raw("PRAGMA analysis_limit=400");
-        await knex.raw("PRAGMA optimize;");
-        LOG.debug("Done optimizing database");
-    }
+
 
     LOG.info(`Scheduling database optimization every 60 minutes.`);
-    setInterval(optimize, 6 * 60 * 60 * 1000);
+    //setInterval(optimize, 6 * 60 * 60 * 1000);
     return orm;
 }
